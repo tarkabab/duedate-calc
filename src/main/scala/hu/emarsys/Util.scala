25baba6ce38d1270fcbfd518fe418a98bc153ed1
gyorgyan <gyorgyan.peter@gmail.com>
2015-11-03 00:33:53 +0100

    add addWholeDays method, minor name refactor

--- src/main/scala/hu/emarsys/Util.scala
+++ src/main/scala/hu/emarsys/Util.scala
@@ -4,6 +4,7 @@ import org.joda.time.{Period, DateTime}
 import org.joda.time.DateTimeConstants._
 
 object Util {
+
   def calculateDueDate(submitDate: String, turnaroundTime: Int): String = {
 
     val start: DateTime = DateTime.parse(submitDate)
@@ -14,14 +15,22 @@ object Util {
     require(workHourEnd >= 0 && workHourEnd < 24 && workHourEnd > workHourStart)
     val workHoursPerDay = workHourEnd - workHourStart
 
-    val completeWorkingDays = turnaroundTime / workHoursPerDay
-    val remainingWorkHours = turnaroundTime % workHoursPerDay
+    val wholeDays = turnaroundTime / workHoursPerDay
+    val remainingHours = turnaroundTime % workHoursPerDay
 
-    def addCompleteWorkingDays(start: DateTime, days: Int) = {
-      // todo: implement this
-      start
+    def addWholeDays(start: DateTime, days: Int) = {
+      val workingDaysPerWeek = 5
+      val daysPerWeek = 7
+
+      val startDay = start.getDayOfWeek
+      val weeks = days / workingDaysPerWeek
+      val remainingDays = days % workingDaysPerWeek
+      val weekOverflow = if((startDay + remainingDays) < SATURDAY) 0 else 2
+      val daysWithWeekends = weeks * daysPerWeek + remainingDays + weekOverflow
+
+      start.plusDays(daysWithWeekends)
     }
-    def addRemainingWorkHours(start: DateTime, hours: Int): DateTime = {
+    def addRemainingHours(start: DateTime, hours: Int): DateTime = {
       require(hours < workHoursPerDay)
       val endOfWork = start.withTime(workHourEnd,0,0,0)
       val end = start.plusHours(hours)
@@ -40,9 +49,8 @@ object Util {
       case _ => day
     }
 
-
-    val startPlusCompleteWorkingDays: DateTime = addCompleteWorkingDays(start, completeWorkingDays)
-    val dueDate = addRemainingWorkHours(startPlusCompleteWorkingDays, remainingWorkHours)
+    val startPlusWholeDays: DateTime = addWholeDays(start, wholeDays)
+    val dueDate = addRemainingHours(startPlusWholeDays, remainingHours)
 
     dueDate.toString
   }
